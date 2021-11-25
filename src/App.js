597bcfd3ca9690e39d7100780426724b6459973e
daniel <heesu07@gmail.com>
2021-11-26 12:16:38 +1300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import Create from './pages/Create'
+
+function App() {
+  return (
+    <div className="App">
+      <Create></Create>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Create.js
@@ -0,0 +1,16 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const Create = (props) => {
+  return(
+      <div>
+        <Typography
+          varient='h1'
+          color='primary'
+        >
+          Create a New Note
+        </Typography>
+      </div>
+  );}
+
+export default Create;
